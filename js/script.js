7d13a83931323796c0ca837577d3a627b5e366b1
8mileverse <Ayodelu4@gmail.com>
2024-10-04 01:02:42 +0100

    completed for now

--- js/script.js
+++ js/script.js
@@ -186,6 +186,86 @@ function displayMovieBackground(type, backdropPath) {
   }
 }
 
+// display TV Details
+function displayShowBackground(type, backdropPath) {
+  const overlaydiv = document.createElement("div");
+  overlaydiv.style.backgroundImage = `url("https://image.tmdb.org/t/p/original/${backdropPath})`;
+  overlaydiv.style.backgroundSize = 'cover';
+  overlaydiv.style.backgroundPosition = 'center';
+  overlaydiv.style.backgroundRepeat = 'no-repeat';
+  overlaydiv.style.height = '100vh';
+  overlaydiv.style.width = '100vw';
+  overlaydiv.style.position = 'absolute';
+  overlaydiv.style.top = '0';
+  overlaydiv.style.left = '0';
+  overlaydiv.style.zIndex = '-1';
+  overlaydiv.style.opacity = '0.3';
+
+  if (type == 'tv') {
+   document.querySelector('#show-details').appendChild(overlaydiv)
+  }
+  else{
+    document.querySelector('#movie-details').appendChild(overlaydiv)
+  }
+}
+
+
+// display TV Details
+async function displayShowDetails() {
+  const showId = window.location.search.split("=")[1];
+  const show = await fetchAPIData(`tv/${showId}}`);
+
+  // overlay for background image
+  displayShowBackground('tv', show.backdrop_path);
+  
+  const div = document.createElement("div");
+
+  // // Create seasons list
+  // const seasonsList = show.seasons
+  //   .map((season) => `<li>${season.name} (${season.episode_count} episodes)</li>`)
+  //   .join("");       <li><span class="text-secondary">Seasons:</span> ${seasonsList} </li>
+
+  div.innerHTML = `
+  <div class="details-top">
+    <div>
+      ${
+        show.poster_path
+          ? `<img src="https://image.tmdb.org/t/p/w500${show.poster_path}" class="card-img-top" alt="${show.name}" />`
+          : `<img src="../images/no-image.jpg" class="card-img-top" alt="${show.name}" />`
+      }
+    </div>
+    <div>
+      <h2>${show.name}</h2>
+      <p>
+        <i class="fas fa-star text-primary"></i>
+        ${show.vote_average.toFixed(1)} / 10
+      </p>
+      <p class="text-muted">Release Date: ${show.first_air_date}</p>
+      <p>${show.overview}</p>
+      <h5>Genres</h5>
+      <ul class="list-group">
+        ${show.genres.map((genre) => `<li>${genre.name}</li>`).join("")}
+      </ul>
+      <a href="${show.homepage}" target="_blank" class="btn">Visit Show Homepage</a>
+    </div>
+  </div>
+  <div class="details-bottom">
+    <h2>Show Info</h2>
+    <ul>
+      <li><span class="text-secondary">Number Of Episodes:</span> ${show.number_of_episodes}</li>
+      <li><span class="text-secondary">Status:</span> ${show.status}</li>
+    </ul>
+    <h4>Production Companies</h4>
+    <div class="list-group">
+      ${show.production_companies.map((company) => `<span>${company.name}</span>`).join(", ")}
+    </div>
+  </div>
+  `;
+  
+
+  document.querySelector("#show-details").appendChild(div);
+}
+
 // Global Fetching From API DataBase
 async function fetchAPIData(endpoint) {
   const API_KEY = "63b797eecf604f8d910938f3a5f3f2db";
@@ -217,7 +297,6 @@ function highlightActiveLink() {
 }
 
 // adding the comma's to the figures
-
 function addCommasToNumber(figure) {
   const number = figure.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","); //regex
   return number;
@@ -239,8 +318,8 @@ function init() {
       displayMovieDetails();
       break;
 
-    case "/tv--details.html":
-      console.log("TV Shows");
+    case "/tv-details.html":
+      displayShowDetails()
       break;
 
     case "/shows.html":
